e20512630edffde9678be3c0fb23a809c64a2f5c
Anton <antony.adamson@gmail.com>
2023-04-30 00:16:04 +0600

    Set Up Project and Display Mode

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,34 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Антон Адамсон on 29.04.2023.
+//
+
+import UIKit
+
+struct ToDo: Equatable {
+    let id = UUID()
+    var title: String
+    var isComplete: Bool
+    var dueDate: Date
+    var notes: String?
+    
+    static func ==(lhs: ToDo, rhs: ToDo) -> Bool {
+        return lhs.id == rhs.id
+    }
+    
+    //Статический метод, который извлекает массив элементов, хранимых на диске, если они есть, и возвращает их. На данный момент заполнен возврат nil
+    static func loadToDos() -> [ToDo]? {
+        return nil
+    }
+    
+    //Статический метод, который заполнит массив примерными данными.
+    static func loadSampleToDos() -> [ToDo] {
+        let toDo1 = ToDo(title: "To-Do One", isComplete: false, dueDate: Date(), notes: "Note 1")
+        let toDo2 = ToDo(title: "To-Do Two", isComplete: false, dueDate: Date(), notes: "Note 2")
+        let toDo3 = ToDo(title: "To-Do Three", isComplete: false, dueDate: Date(), notes: "Note 3")
+        
+        return [toDo1, toDo2, toDo3]
+    }
+}
---
+++ ToDoList/ToDoTableViewController.swift
@@ -0,0 +1,94 @@
+//
+//  ToDoTableViewController.swift
+//  ToDoList
+//
+//  Created by Антон Адамсон on 29.04.2023.
+//
+
+import UIKit
+
+class ToDoTableViewController: UITableViewController {
+
+    var toDos = [ToDo]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //проверка наличия сохраненных данных при первом запуске приложения, если их нет, подгрузить пример
+        if let savedToDos = ToDo.loadToDos() {
+            toDos = savedToDos
+        } else {
+            toDos = ToDo.loadSampleToDos()
+        }
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return toDos.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCellIdentifier", for: indexPath)
+
+        let toDo = toDos[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = toDo.title
+        cell.contentConfiguration = content
+        
+        return cell
+    }
+
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
